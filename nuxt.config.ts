2c2a38eb9b6477a9f8123384a103d55f843ed5da
s1naf <theofaniskavvadias@gmail.com>
2025-10-26 14:21:50 +0200

    Config for github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  app: {
+    baseURL: '/soutane-app/',
+  },
+  
   modules: [
     '@nuxt/image',
     'nuxt-swiper'
