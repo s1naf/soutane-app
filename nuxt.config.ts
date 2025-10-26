54f6ab48c424ef8123741377168837097da5ec7c
s1naf <theofaniskavvadias@gmail.com>
2025-10-26 14:39:59 +0200

    Config for IOGIT

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image'],
+  // Σημαντικό για GitHub Pages project site: user.github.io/soutane-app/
+  app: {
+    baseURL: '/soutane-app/'
+  },
+  // Παραγωγή στατικών αρχείων με σωστές ρυθμίσεις για GitHub Pages
+  nitro: {
+    preset: 'github_pages'
+  }
 })
